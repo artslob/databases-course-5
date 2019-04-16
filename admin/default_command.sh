ea5cb9a6f7a4347dbdf51bf4db92d64d728a6119
artslob <artslob@yandex.ru>
2019-04-16 17:41:28 +0300

    uncomment commands

--- admin/default_command.sh
+++ admin/default_command.sh
@@ -2,6 +2,7 @@
 
 # TODO wait for databases to load
 while :; do sleep 100; done
-#psql -U postgres -h pg -c "SELECT * FROM person LIMIT 1;";
-#mysql -u root -hmysql uni -e "SELECT * FROM uni.person;"
-#mongo mongo/uni --eval "printjson(db.getCollectionNames())"
+
+psql -U postgres -h pg -c "SELECT * FROM person LIMIT 1;";
+mysql -u root -hmysql uni -e "SELECT * FROM uni.person;"
+mongo mongo/uni --eval "printjson(db.getCollectionNames())"
