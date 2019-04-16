213effab8ec18269bc3209ba6b72309213cd16f8
artslob <artslob@yandex.ru>
2019-04-16 14:52:16 +0300

    default command for admin - sleep

--- admin/default_command.sh
+++ admin/default_command.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO wait for databases to load
-psql -U postgres -h pg -c "SELECT * FROM person LIMIT 1;";
+while :; do sleep 100; done
+#psql -U postgres -h pg -c "SELECT * FROM person LIMIT 1;";
 #mysql -u root -hmysql uni -e "SELECT * FROM uni.person;"
 #mongo mongo/uni --eval "printjson(db.getCollectionNames())"
