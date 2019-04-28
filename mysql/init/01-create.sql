afa6a43adf5e181ce038ace8754c9bfd1dd1a9e0
artslob <artslob@yandex.ru>
2019-04-28 18:52:42 +0300

    mysql: change format of comments from # to --

--- mysql/init/01-create.sql
+++ mysql/init/01-create.sql
@@ -8,88 +8,88 @@ CREATE TABLE person
     name        VARCHAR(20)                          NOT NULL,
     surname     VARCHAR(20)                          NOT NULL,
     middle_name VARCHAR(20),
-    position    ENUM ('доцент','бакалавр','магистр') NOT NULL # должность
+    position    ENUM ('доцент','бакалавр','магистр') NOT NULL -- должность
 );
 
 CREATE TABLE reader_list
 (
-    list_id           INT PRIMARY KEY AUTO_INCREMENT, # Читательский лист
-    registration_date DATE       NOT NULL,            # дата регистрации
+    list_id           INT PRIMARY KEY AUTO_INCREMENT, -- Читательский лист
+    registration_date DATE       NOT NULL,            -- дата регистрации
     person_id         INT UNIQUE NOT NULL REFERENCES person (person_id)
 );
 
 CREATE TABLE book
 (
     book_id          INT PRIMARY KEY AUTO_INCREMENT,
-    title            VARCHAR(100) NOT NULL, # название
-    author           VARCHAR(100) NOT NULL, # автор
-    publication_date DATE                   # Дата публикации
+    title            VARCHAR(100) NOT NULL, -- название
+    author           VARCHAR(100) NOT NULL, -- автор
+    publication_date DATE                   -- Дата публикации
 );
 
 CREATE TABLE reader_info
 (
     info_id     INT PRIMARY KEY AUTO_INCREMENT,
-    reader_id   INT  NOT NULL REFERENCES reader_list (list_id), # человек
-    book_id     INT  NOT NULL REFERENCES book (book_id),        # взял книгу
-    obtain_date DATE NOT NULL,                                  # когда взял
-    returned    DATE                                            # когда вернул
+    reader_id   INT  NOT NULL REFERENCES reader_list (list_id), -- человек
+    book_id     INT  NOT NULL REFERENCES book (book_id),        -- взял книгу
+    obtain_date DATE NOT NULL,                                  -- когда взял
+    returned    DATE                                            -- когда вернул
 );
 
 CREATE TABLE conference
 (
     conference_id INT PRIMARY KEY AUTO_INCREMENT,
-    title         VARCHAR(100) NOT NULL, # Наименование конференции
-    place         VARCHAR(100) NOT NULL, # Место проведения конференции
-    # TODO rename to conference_date
-    date          DATE         NOT NULL  # Дата конференции
+    title         VARCHAR(100) NOT NULL, -- Наименование конференции
+    place         VARCHAR(100) NOT NULL, -- Место проведения конференции
+    -- TODO rename to conference_date
+    date          DATE         NOT NULL  -- Дата конференции
 );
 
-CREATE TABLE conference_participants # участники конференции
+CREATE TABLE conference_participants -- участники конференции
 (
-    conference_id INT NOT NULL REFERENCES conference (conference_id), # конференция
-    person_id     INT NOT NULL REFERENCES person (person_id),         # участник
+    conference_id INT NOT NULL REFERENCES conference (conference_id), -- конференция
+    person_id     INT NOT NULL REFERENCES person (person_id),         -- участник
     PRIMARY KEY (conference_id, person_id)
 );
 
 CREATE TABLE project
 (
     project_id INT PRIMARY KEY AUTO_INCREMENT,
-    title      VARCHAR(100) NOT NULL, # Наименование научного проекта
-    # TODO rename it to project_start and project_end
-    start      DATE         NOT NULL, # Период участия в проекте
+    title      VARCHAR(100) NOT NULL, -- Наименование научного проекта
+    -- TODO rename it to project_start and project_end
+    start      DATE         NOT NULL, -- Период участия в проекте
     end        DATE         NOT NULL
 );
 
-CREATE TABLE project_participants # участники проекта
+CREATE TABLE project_participants -- участники проекта
 (
-    project_id INT NOT NULL REFERENCES project (project_id), # научный проект
-    person_id  INT NOT NULL REFERENCES person (person_id),   # участник
+    project_id INT NOT NULL REFERENCES project (project_id), -- научный проект
+    person_id  INT NOT NULL REFERENCES person (person_id),   -- участник
     PRIMARY KEY (project_id, person_id)
 );
 
 CREATE TABLE publication
 (
     publication_id INT PRIMARY KEY AUTO_INCREMENT,
-    title          VARCHAR(100) NOT NULL,     # Наименование публикации
-    type           ENUM ('статья', 'тезисы'), # статья/тезисы
-    citation_index INT                        # Индекс цитирования
+    title          VARCHAR(100) NOT NULL,     -- Наименование публикации
+    type           ENUM ('статья', 'тезисы'), -- статья/тезисы
+    citation_index INT                        -- Индекс цитирования
 );
 
-CREATE TABLE publication_coauthors # соавторы публикации
+CREATE TABLE publication_coauthors -- соавторы публикации
 (
-    publication_id INT NOT NULL REFERENCES publication (publication_id), # публикация
-    person_id      INT NOT NULL REFERENCES person (person_id),           # соавтор
+    publication_id INT NOT NULL REFERENCES publication (publication_id), -- публикация
+    person_id      INT NOT NULL REFERENCES person (person_id),           -- соавтор
     PRIMARY KEY (publication_id, person_id)
 );
 
-CREATE TABLE edition # издание
+CREATE TABLE edition -- издание
 (
     edition_id     INT PRIMARY KEY AUTO_INCREMENT,
-    title          VARCHAR(100) NOT NULL, # Наименование издания
-    language       VARCHAR(30)  NOT NULL, # Язык издания
-    volume         INT          NOT NULL, # Объём издания -- 5 листов
-    place          VARCHAR(100) NOT NULL, # Место издания -- Тверь/Москва
-    type           VARCHAR(50)  NOT NULL, # Тип издания -- вак, ринц ....
+    title          VARCHAR(100) NOT NULL, -- Наименование издания
+    language       VARCHAR(30)  NOT NULL, -- Язык издания
+    volume         INT          NOT NULL, -- Объём издания -- 5 листов
+    place          VARCHAR(100) NOT NULL, -- Место издания -- Тверь/Москва
+    type           VARCHAR(50)  NOT NULL, -- Тип издания -- вак, ринц ....
     publication_id INT          NOT NULL REFERENCES publication (publication_id)
 );
 
