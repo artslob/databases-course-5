40e5010720113056a8b577adb82be9722af957dc
dadle8 <dadly8@gmail.com>
2019-04-28 17:10:07 +0300

    Delete boiler code for Postgres entities

--- database/src/main/java/ru/ifmo/database/entity/postgres/PostgresDepartment.java
+++ database/src/main/java/ru/ifmo/database/entity/postgres/PostgresDepartment.java
@@ -1,105 +1,36 @@
 package ru.ifmo.database.entity.postgres;
 
+import lombok.*;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
 @Table(name = "department", schema = "public", catalog = "ifmo_hibd")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class PostgresDepartment {
-    private int departmentId;
-    private String name;
-    private String educationalForm;
-    private String standard;
-    private String faculty;
-    private int universityId;
-
     @Id
-    @Column(name = "department_id", nullable = false)
-    public int getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(int departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    @Basic
-    @Column(name = "name", nullable = false, length = 100)
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Basic
-    @Column(name = "educational_form", nullable = false, length = 100)
-    public String getEducationalForm() {
-        return educationalForm;
-    }
-
-    public void setEducationalForm(String educationalForm) {
-        this.educationalForm = educationalForm;
-    }
-
-    @Basic
-    @Column(name = "standard", nullable = false, length = 10)
-    public String getStandard() {
-        return standard;
-    }
-
-    public void setStandard(String standard) {
-        this.standard = standard;
-    }
-
-    @Basic
-    @Column(name = "faculty", nullable = false, length = 100)
-    public String getFaculty() {
-        return faculty;
-    }
-
-    public void setFaculty(String faculty) {
-        this.faculty = faculty;
-    }
-
-    @Basic
-    @Column(name = "university_id", nullable = false)
-    public int getUniversityId() {
-        return universityId;
-    }
-
-    public void setUniversityId(int universityId) {
-        this.universityId = universityId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PostgresDepartment that = (PostgresDepartment) o;
-        return departmentId == that.departmentId &&
-                universityId == that.universityId &&
-                Objects.equals(name, that.name) &&
-                Objects.equals(educationalForm, that.educationalForm) &&
-                Objects.equals(standard, that.standard) &&
-                Objects.equals(faculty, that.faculty);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(departmentId, name, educationalForm, standard, faculty, universityId);
-    }
-
-    @Override
-    public String toString() {
-        return "PostgresDepartment{" +
-                "departmentId=" + departmentId +
-                ", name='" + name + '\'' +
-                ", educationalForm='" + educationalForm + '\'' +
-                ", standard='" + standard + '\'' +
-                ", faculty='" + faculty + '\'' +
-                ", universityId=" + universityId +
-                '}';
-    }
+    @Column(name = "department_id")
+    private int departmentId;
+    @NotNull
+    @Size(max = 100)
+    private String name;
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "educational_form")
+    private String educationalForm;
+    @NotNull
+    @Size(max = 10)
+    private String standard;
+    @NotNull
+    @Size(max = 100)
+    private String faculty;
+    @Column(name = "university_id")
+    private int universityId;
 }
--- database/src/main/java/ru/ifmo/database/entity/postgres/PostgresPerson.java
+++ database/src/main/java/ru/ifmo/database/entity/postgres/PostgresPerson.java
@@ -1,105 +1,35 @@
 package ru.ifmo.database.entity.postgres;
 
+import lombok.*;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
 @Table(name = "person", schema = "public", catalog = "ifmo_hibd")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class PostgresPerson {
-    private int personId;
-    private String name;
-    private String surname;
-    private String middleName;
-    private String personType;
-    private Integer departmentId;
-
     @Id
-    @Column(name = "person_id", nullable = false)
-    public int getPersonId() {
-        return personId;
-    }
-
-    public void setPersonId(int personId) {
-        this.personId = personId;
-    }
-
-    @Basic
-    @Column(name = "name", nullable = false, length = 20)
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Basic
-    @Column(name = "surname", nullable = false, length = 20)
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Basic
-    @Column(name = "middle_name", nullable = true, length = 20)
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    @Basic
-    @Column(name = "person_type", nullable = false)
-    public String getPersonType() {
-        return personType;
-    }
-
-    public void setPersonType(String personType) {
-        this.personType = personType;
-    }
-
-    @Basic
-    @Column(name = "department_id", nullable = true)
-    public Integer getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Integer departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PostgresPerson that = (PostgresPerson) o;
-        return personId == that.personId &&
-                Objects.equals(name, that.name) &&
-                Objects.equals(surname, that.surname) &&
-                Objects.equals(middleName, that.middleName) &&
-                Objects.equals(personType, that.personType) &&
-                Objects.equals(departmentId, that.departmentId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(personId, name, surname, middleName, personType, departmentId);
-    }
-
-    @Override
-    public String toString() {
-        return "PostgresPerson{" +
-                "personId=" + personId +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", middleName='" + middleName + '\'' +
-                ", personType=" + personType +
-                ", departmentId=" + departmentId +
-                '}';
-    }
+    @Column(name = "person_id")
+    private int personId;
+    @NotNull
+    @Size(max = 20)
+    private String name;
+    @NotNull
+    @Size(max = 20)
+    private String surname;
+    @NotNull
+    @Size(max = 20)
+    @Column(name = "middle_name")
+    private String middleName;
+    @Column(name = "person_type")
+    private String personType;
+    @Column(name = "department_id")
+    private Integer departmentId;
 }
