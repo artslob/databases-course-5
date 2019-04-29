b9cb52a244eb0c49adc75fc62c0053cd0a1255b5
artslob <artslob@yandex.ru>
2019-04-29 21:46:01 +0300

    pg: add inserts

--- postgres/init/01-create.sql
+++ postgres/init/01-create.sql
@@ -84,5 +84,86 @@ CREATE TABLE work_discipline
     PRIMARY KEY (work_id, discipline_id)
 );
 
-INSERT INTO person(name, surname, middle_name, person_type)
-VALUES ('Иван', 'Иванов', 'Иванович', 'student');
+INSERT INTO person(person_id, name, surname, middle_name, person_type)
+VALUES -- students
+       (191950, 'Ivan', 'Ivanov', 'Ivanovich', 'student'),
+       (191951, 'Fedor', 'Fedorov', 'Fedorovich', 'student'),
+       (191952, 'Aleksey', 'Alekseev', 'Alekseevich', 'student'),
+       -- professors
+       (75950, 'Dmitriy', 'Dmitriev', 'Dmitrievich', 'professor'),
+       (75951, 'Artem', 'Artemiev', 'Artemievich', 'professor'),
+       (75952, 'Kirill', 'Kirillov', 'Kirillovich', 'professor');
+
+INSERT INTO university(university_id, name)
+VALUES (1, 'ITMO'),
+       (2, 'Saint Petersburg State University'),
+       (3, 'Peter the Great St.Petersburg Polytechnic University');
+
+INSERT INTO department(department_id, faculty, university_id)
+VALUES -- itmo
+       (1, 'Faculty of Software Engineering and Computer Systems', 1),
+       (2, 'Faculty of Secure Information Technologies', 1),
+       (3, 'Institute of Information and Navigation Systems', 1),
+       -- spsu
+       (4, 'Graduate School of Journalism and Mass Communication', 2),
+       (5, 'Graduate School of Management', 2),
+       (6, 'Institute of Earth Sciences', 2),
+       -- spbpu
+       (7, 'Department for Protecting Confidential Information', 3),
+       (8, 'Safety and Security Office', 3),
+       (9, 'Technopark “Polytechnic”', 3);
+
+INSERT INTO speciality (speciality_id, name, standard, department_id)
+VALUES (1, '01.01.01', 'new', 1),
+       (2, '01.01.02', 'new', 2),
+       (3, '01.01.03', 'new', 3),
+       (4, '02.01.01', 'old', 4),
+       (5, '02.01.02', 'old', 5),
+       (6, '02.01.03', 'old', 6),
+       (7, '03.01.01', 'old', 7),
+       (8, '03.01.02', 'old', 8),
+       (9, '03.01.03', 'old', 9);
+
+INSERT INTO discipline (discipline_id, name, lectures, practical_lessons, labs, control_form, semester, speciality_id)
+VALUES (1, 'graphics', 17, 17, 17, 'exam', 1, 1),
+       (2, 'computers', 17, 17, 17, 'exam', 1, 2),
+       (3, 'c/c++', 17, 17, 17, 'exam', 1, 3),
+       (4, 'machine learning', 35, 35, 35, 'test', 2, 4),
+       (5, 'ai', 35, 35, 35, 'test', 2, 5),
+       (6, 'cryptography', 35, 35, 35, 'test', 2, 6),
+       (7, 'java', 17, 17, 17, 'exam', 3, 7),
+       (8, 'asm', 17, 17, 17, 'exam', 3, 8),
+       (9, 'iso', 17, 17, 17, 'exam', 3, 9);
+
+INSERT INTO grade(grade_id, grade, grade_date, person_id, discipline_id)
+VALUES (1, 91, '2013-06-01', 191950, 1),
+       (2, 92, '2013-06-02', 191950, 2),
+       (3, 93, '2013-06-03', 191950, 3),
+       (4, 94, '2013-06-04', 191951, 4),
+       (5, 95, '2013-06-05', 191951, 5),
+       (6, 96, '2013-06-06', 191951, 6),
+       (7, 97, '2013-06-07', 191952, 7),
+       (8, 98, '2013-06-08', 191952, 8),
+       (9, 99, '2013-06-09', 191952, 9);
+
+INSERT INTO education (education_id, education_type, education_form, qualification, start_date, end_date, course,
+                       speciality_id, person_id)
+VALUES (1, 'budget', 'distant', 'master', '2012-09-01', NULL, 1, 1, 191950),
+       (2, 'budget', 'distant', 'master', '2012-09-01', NULL, 1, 1, 191950),
+       (3, 'budget', 'distant', 'master', '2012-09-01', NULL, 1, 1, 191950),
+       (4, 'budget', 'distant', 'master', '2012-09-01', NULL, 2, 2, 191951),
+       (5, 'budget', 'distant', 'master', '2012-09-01', NULL, 2, 2, 191951),
+       (6, 'budget', 'distant', 'master', '2012-09-01', NULL, 2, 2, 191951),
+       (7, 'budget', 'distant', 'master', '2012-09-01', NULL, 3, 3, 191952),
+       (8, 'budget', 'distant', 'master', '2012-09-01', NULL, 3, 3, 191952),
+       (9, 'budget', 'distant', 'master', '2012-09-01', NULL, 3, 3, 191952);
+
+INSERT INTO work (work_id, position, work_start, work_end, person_id)
+VALUES (1, 'professor', '1999-06-01', NULL, 75950),
+       (2, 'professor', '1999-06-02', NULL, 75951),
+       (3, 'professor', '1999-06-03', NULL, 75952);
+
+INSERT INTO work_discipline(work_id, discipline_id)
+VALUES (1, 1),
+       (2, 3),
+       (3, 3);
