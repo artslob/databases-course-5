16acfd154475e955dca1dcff41265d3d45d640d7
dadle8 <dadly8@gmail.com>
2019-05-06 17:55:04 +0300

    Add MergeReaderListService

---
+++ database/src/main/java/ru/ifmo/database/repository/union/UnionReaderListRepository.java
@@ -0,0 +1,7 @@
+package ru.ifmo.database.repository.union;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.ifmo.database.entity.union.UnionReaderList;
+
+public interface UnionReaderListRepository extends CrudRepository<UnionReaderList, Integer> {
+}
