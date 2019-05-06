0da9aa6d2b6f0d99360db0a6c65d6c7658ce37bf
b1nt_ <slava_tikhonov@outlook.com>
2019-05-06 11:12:11 +0300

    Add MergeSpecialityService

---
+++ database/src/main/java/ru/ifmo/database/repository/union/UnionSpecialityRepository.java
@@ -0,0 +1,7 @@
+package ru.ifmo.database.repository.union;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.ifmo.database.entity.union.UnionSpeciality;
+
+public interface UnionSpecialityRepository extends CrudRepository<UnionSpeciality, Integer> {
+}
