8470de577378667f5b462fd3289fc6cdf6f08805
b1nt_ <slava_tikhonov@outlook.com>
2019-05-06 11:12:11 +0300

    Add MergeSpecialityService

--- database/src/main/java/ru/ifmo/database/controller/AllController.java
+++ database/src/main/java/ru/ifmo/database/controller/AllController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.ifmo.database.entity.union.UnionDepartment;
 import ru.ifmo.database.entity.union.UnionPerson;
+import ru.ifmo.database.entity.union.UnionSpeciality;
 import ru.ifmo.database.entity.union.UnionUniversity;
 import ru.ifmo.database.etl.impl.MergeDepartmentService;
 import ru.ifmo.database.etl.impl.MergePersonService;
+import ru.ifmo.database.etl.impl.MergeSpecialityService;
 import ru.ifmo.database.etl.impl.MergeUniversityService;
 
 import java.util.List;
@@ -19,11 +21,13 @@ public class AllController {
     private final MergePersonService mergePersonService;
     private final MergeUniversityService mergeUniversityService;
     private final MergeDepartmentService mergeDepartmentService;
+    private final MergeSpecialityService mergeSpecialityService;
 
-    public AllController(MergePersonService mergePersonService, MergeUniversityService mergeUniversityService, MergeDepartmentService mergeDepartmentService) {
+    public AllController(MergePersonService mergePersonService, MergeUniversityService mergeUniversityService, MergeDepartmentService mergeDepartmentService, MergeSpecialityService mergeSpecialityService) {
         this.mergePersonService = mergePersonService;
         this.mergeUniversityService = mergeUniversityService;
         this.mergeDepartmentService = mergeDepartmentService;
+        this.mergeSpecialityService = mergeSpecialityService;
     }
 
     @PostMapping("/merge/all")
@@ -32,6 +36,7 @@ public class AllController {
             mergePerson();
             mergeUniversity();
             mergeDepartment();
+            mergeSpeciality();
             return "Success";
         } catch (Exception ex) {
             ex.getStackTrace();
@@ -53,4 +58,10 @@ public class AllController {
     public List<UnionDepartment> mergeDepartment() {
         return mergeDepartmentService.merge();
     }
+
+    @PostMapping("/merge/speciality")
+    public List<UnionSpeciality> mergeSpeciality() {
+        return mergeSpecialityService.merge();
+    }
+
 }
---
+++ database/src/main/java/ru/ifmo/database/etl/impl/MergeSpecialityService.java
@@ -0,0 +1,58 @@
+package ru.ifmo.database.etl.impl;
+
+import org.springframework.stereotype.Service;
+import ru.ifmo.database.entity.oracle.OracleSpeciality;
+import ru.ifmo.database.entity.postgres.PostgresSpeciality;
+import ru.ifmo.database.entity.union.UnionDepartment;
+import ru.ifmo.database.entity.union.UnionSpeciality;
+import ru.ifmo.database.etl.api.AbstractMergeService;
+import ru.ifmo.database.etl.datamodel.ExtractTwoData;
+import ru.ifmo.database.repository.oracle.OracleSpecialityRepository;
+import ru.ifmo.database.repository.postgres.PostgresSpecialityRepository;
+import ru.ifmo.database.repository.union.UnionSpecialityRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MergeSpecialityService extends AbstractMergeService<ExtractTwoData<PostgresSpeciality, OracleSpeciality>, UnionSpeciality> {
+
+    private final PostgresSpecialityRepository postgresRepository;
+    private final OracleSpecialityRepository oracleRepository;
+    private final UnionSpecialityRepository unionRepository;
+
+    public MergeSpecialityService(PostgresSpecialityRepository postgresRepository, OracleSpecialityRepository oracleRepository, UnionSpecialityRepository unionRepository) {
+        this.postgresRepository = postgresRepository;
+        this.oracleRepository = oracleRepository;
+        this.unionRepository = unionRepository;
+    }
+
+    public ExtractTwoData<PostgresSpeciality, OracleSpeciality> extract() {
+        List<PostgresSpeciality> postgresList = (List<PostgresSpeciality>) postgresRepository.findAll();
+        List<OracleSpeciality> oracleList = (List<OracleSpeciality>) oracleRepository.findAll();
+
+        postgresList.forEach(p -> oracleList.add(oracleRepository.findById(p.getSpecialityId()).orElse(new OracleSpeciality())));
+
+        return new ExtractTwoData<>(postgresList, oracleList);
+    }
+
+    public List<UnionSpeciality> transform(ExtractTwoData<PostgresSpeciality, OracleSpeciality> extractData) {
+        List<UnionSpeciality> unionList = new ArrayList<>();
+        for (int i = 0; i < extractData.getPostgresEntityList().size(); i++) {
+            UnionSpeciality unionSpeciality = new UnionSpeciality();
+            unionSpeciality.setSpecialityId(extractData.getPostgresEntityList().get(i).getSpecialityId());
+            unionSpeciality.setName(extractData.getPostgresEntityList().get(i).getName());
+            unionSpeciality.setStandard(extractData.getPostgresEntityList().get(i).getStandard());
+            unionSpeciality.setDepartmentId(extractData.getPostgresEntityList().get(i).getDepartmentId());
+
+            unionSpeciality.setDirection(extractData.getOracleEntityList().get(i).getDirection());
+
+            unionList.add(unionSpeciality);
+        }
+        return unionList;
+    }
+
+    public List<UnionSpeciality> load(List<UnionSpeciality> unionList) {
+        return (List<UnionDepartment>) unionRepository.saveAll(unionList);
+    }
+}
