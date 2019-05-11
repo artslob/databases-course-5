7ec6eae166788965c1ded6d50f786671527fc194
artslob <artslob@yandex.ru>
2019-05-11 15:22:39 +0300

    rewrite insert using classes

--- olap-oracle/generate_insert.py
+++ olap-oracle/generate_insert.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-birthplaces = {
+birthplaces_dict = {
     'Russia': {
         'Vologda Oblast': ['Vologda', 'Cherepovets', 'Sokol'],
         'Yaroslavl Oblast': ['Yaroslavl', 'Rostov', 'Rybinsk', 'Uglich'],
@@ -13,54 +13,85 @@ birthplaces = {
 }
 
 
+class Birthplace:
+    def __init__(self, birthplace_id, country_key, country_name, region_key, region_name, city_key, city_name):
+        self.birthplace_id = birthplace_id
+        self.country_key = country_key
+        self.country_name = country_name
+        self.region_key = region_key
+        self.region_name = region_name
+        self.city_key = city_key
+        self.city_name = city_name
+
+    def __str__(self):
+        return '\n'.join([
+            f'INSERT INTO birthplace (birthplace_id, country_key, country_name, region_key, region_name, city_key, city_name)',
+            f"VALUES ({self.birthplace_id}, {self.country_key}, '{self.country_name}', {self.region_key}, '{self.region_name}', {self.city_key}, '{self.city_name}');"
+        ])
+
+
 def generate_birthplaces():
-    lines = []
-    t_id = 1
-    for i, county in enumerate(birthplaces.keys(), 1):
-        for j, region in enumerate(birthplaces[county].keys(), 1):
-            for k, city in enumerate(birthplaces[county][region], 1):
-                lines.extend([
-                    f'INSERT INTO birthplace (birthplace_id, country_key, country_name, region_key, region_name, city_key, city_name)',
-                    f"VALUES ({t_id}, {i}, '{county}', {j}, '{region}', {k}, '{city}');"
-                ])
-                t_id += 1
-    return '\n'.join(lines)
+    birthplaces_table = []
+    birthplace_id = 1
+    for i, county in enumerate(birthplaces_dict.keys(), 1):
+        for j, region in enumerate(birthplaces_dict[county].keys(), 1):
+            for k, city in enumerate(birthplaces_dict[county][region], 1):
+                birthplaces_table.append(Birthplace(birthplace_id, i, county, j, region, k, city))
+                birthplace_id += 1
+    return birthplaces_table
+
+
+class Time:
+    def __init__(self, time_id, year_key, year_name, term_key, term_name):
+        self.time_id = time_id
+        self.year_key = year_key
+        self.year_name = year_name
+        self.term_key = term_key
+        self.term_name = term_name
+
+    def __str__(self):
+        return '\n'.join([
+            f'INSERT INTO time_t (time_id, year_key, year_name, term_key, term_name)',
+            f"VALUES ({self.time_id}, {self.year_key}, '{self.year_name}', {self.term_key}, '{self.term_name}');"
+        ])
 
 
 def generate_times():
-    lines = []
+    time_table = []
     t_id = 1
     for year in range(1990, 2000):
         for j in range(1, 3):
-            lines.extend([
-                f'INSERT INTO time_t (time_id, year_key, year_name, term_key, term_name)',
-                f"VALUES ({t_id}, {year}, '{year}', {j}, '{j}');"
-            ])
+            time_table.append(Time(t_id, year, year, j, j))
             t_id += 1
-    return '\n'.join(lines)
+    return time_table
 
 
-cwd = Path().absolute()
-target_file = cwd / 'scripts' / 'insert.sql'
+def write_to_file(target, *strings):
+    if not target.exists():
+        raise ValueError(f"target file '{target}' not exist!")
+
+    with open(target, 'w') as f:
+        f.write('\n'.join(strings) + '\n')
 
 
-def write_to_file():
-    if not target_file.exists():
-        raise ValueError(f"target file '{target_file}' not exist!")
+def main():
+    cwd = Path().absolute()
+    target = cwd / 'scripts' / 'insert.sql'
+    print(f"target file is: '{target}'")
 
-    with open(target_file, 'w') as f:
-        f.write(
-            '\n'.join([
-                generate_birthplaces(),
-                '',
-                generate_times(),
-            ])
-            + '\n'
-        )
+    birthplaces = generate_birthplaces()
+    times = generate_times()
+
+    birthplaces_insert = '\n'.join(str(i) for i in birthplaces)
+    times_insert = '\n'.join(str(i) for i in times)
+
+    print()
+    print(birthplaces_insert)
+    print()
+    print(times_insert)
+
+    write_to_file(target, birthplaces_insert, '', times_insert)
 
 
 if __name__ == '__main__':
-    print(f"target file is: '{target_file}'")
-    print(generate_birthplaces())
-    print(generate_times())
-    write_to_file()
+    main()
